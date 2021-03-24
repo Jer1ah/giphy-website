154daaa4b7e3041263c29ad46ebfc23b7e35006a
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-03-24 19:20:13 -0400

    working on trending carousel

--- src/components/gifItem.jsx
+++ src/components/gifItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import styles from '../css/gifItem.module.css';
+import '../css/gifItem.module.css';
 
 const gifItem = ({ gif }) => {
     return (
-        <li className={styles.gifItem}>
+        <li>
             <img src={gif} alt="Gif Description"/>
         </li>
     );
--- src/components/trendingList.jsx
+++ src/components/trendingList.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import { React, Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 import GifItem from '../components/gifItem';
 import icon from '../images/trend.svg';
 import styles from '../css/trendingList.module.css';
 
-const trendingList = (props) => {
-    const trendingGifList = props.trendingGifs.map((gif) => {
-        return <GifItem gif={gif.images.downsized.url}/>
-    });
+class trendingList extends Component {
+    render() {
+        const trendingGifList = this.props.trendingGifs.map((gif) => {
+            return <GifItem gif={gif.images.downsized.url}/>
+        });
 
-    return (
-        <div className={styles.trending}>
-            <span className={styles.heading}>
-                <img src={icon} alt="trending icon"/>
-                <h3>Trending</h3>
-            </span>
-            <ul className={styles.trendingList}>
-                {trendingGifList}
-            </ul>
-        </div>
-    );
+        return (
+            <div className={styles.trending}>
+                <span className={styles.heading}>
+                    <img src={icon} alt="trending icon"/>
+                    <h3>Trending</h3>
+                </span>
+                <Carousel>
+                    {trendingGifList}
+                </Carousel>
+            </div>
+        );
+    }
 };
 
 export default trendingList;
